8625cc84febda60eca025b519ccba07267fd7af7
VIJAY P <pagoluvijay7@gmail.com>
2025-08-27 09:00:56 +0000

    I THINK U DID NOT CHANGE THE LOGO

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,8 +12,9 @@ export const SaathiIcon = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M12 21a9 9 0 0 0-9-9 9 9 0 0 1 9-9 9 9 0 0 1 9 9 9 9 0 0 0-9 9z" />
-    <path d="M12 3a9 9 0 0 0 9 9" />
-    <path d="M12 21a9 9 0 0 1-9-9" />
+    <path d="M12 5C7.03 5 3 9.03 3 14c0 2.22.89 4.25 2.34 5.74" />
+    <path d="M12 5c4.97 0 9 4.03 9 9 0 2.22-.89 4.25-2.34 5.74" />
+    <path d="M12 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
+    <path d="M18.66 19.74c-1.45-1.49-3.48-2.5-5.66-2.5s-4.21 1.01-5.66 2.5" />
   </svg>
 );
