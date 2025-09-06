570d9d6dcf28cd06c394d92c73d528be21ff34ff
VIJAY P <pagoluvijay7@gmail.com>
2025-09-06 04:26:18 +0000

    module.exports = {   content: [     "./src/app/**/*.{js,ts,jsx,tsx}",

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
+
 import type {Config} from 'tailwindcss';
 import {fontFamily} from 'tailwindcss/defaultTheme';
 
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
